f74b674ba9c5fd63ddebd726b9856951cbc78586
gurpreet-singh135 <gsingh2@cs.iitr.ac.in>
2025-08-25 21:14:47 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -8,11 +8,18 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
+const (
+	CLRF = "\r\n"
+)
+
+var (
+	store = make(map[string]string)
+	storeMutex sync.RWMutex
+)
+
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
@@ -32,6 +39,22 @@ func main() {
 		go handleConnection(conn)
 	}
 
+}
+
+func buildRESPBulkString(s string) string {
+	if s == "" {
+		return "$" + "-1" + CLRF
+	}
+	s = strings.TrimSpace(s)
+	strLen := len(s)
+	RESPString := "$" + strconv.Itoa(strLen) + CLRF + s + CLRF;
+	return RESPString
+}
+
+func buildRESPSimpleString(s string) string {
+	s = strings.TrimSpace(s)
+	RESPString := "+" + s + CLRF;
+	return RESPString	
 }
 
 
@@ -56,31 +79,55 @@ func handleConnection(conn net.Conn) {
 		
 		// Parse RESP array
 		if strings.HasPrefix(line, "*") {
-			count, _ := strconv.Atoi(line[1:])
+			// _, _ = reader.ReadString('\n')
+			// count, _ := strconv.Atoi(line[1:])
 			
-			for i := 0; i < count; i++ {
 				// Read bulk string length
-				lengthLine, _ := reader.ReadString('\n')
-				lengthLine = strings.TrimSpace(lengthLine)
-				fmt.Printf("Length: %s\n", lengthLine)
-				
-				// Read bulk string data
-				dataLine, _ := reader.ReadString('\n')
-				command := strings.TrimSpace(dataLine)
-				fmt.Printf("Command: %s\n", command)
-				
-				switch command {
-				case "PING":
-					conn.Write([]byte("+PONG\r\n"))
-				case "ECHO":
-					lenEchoStr, _ := reader.ReadString('\n')
-					lenEchoStr = strings.TrimSpace(lenEchoStr)
-					log.Println("Length of the echoed string: ", lenEchoStr)
-					echoStr, _ := reader.ReadString('\n')
-					echoStr = strings.TrimSpace(echoStr)
+			lengthLine, _ := reader.ReadString('\n')
+			lengthLine = strings.TrimSpace(lengthLine)
+			fmt.Printf("Length: %s\n", lengthLine)
+			
+			// Read bulk string data
+			dataLine, _ := reader.ReadString('\n')
+			command := strings.TrimSpace(dataLine)
+			fmt.Printf("Command: %s\n", command)
+			
+			switch command {
+			case "PING":
+				conn.Write([]byte("+PONG\r\n"))
+			case "ECHO":
+				_, _ = reader.ReadString('\n')
+				echoStr, _ := reader.ReadString('\n')
+				echoStr = strings.TrimSpace(echoStr)
 
-					conn.Write([]byte(lenEchoStr + "\r\n" + echoStr + "\r\n"))
-				}
+				conn.Write([]byte(buildRESPBulkString(echoStr)))
+			case "GET":
+				log.Println("GET command is executed")
+				_, _ = reader.ReadString('\n')
+				key, _ := reader.ReadString('\n')
+				key = strings.TrimSpace(key)
+				storeMutex.RLock()
+				value := store[key]
+
+				log.Println("GET: key and value are: ", key, value)
+
+				conn.Write([]byte(buildRESPBulkString(value)))
+				storeMutex.RUnlock()
+			case "SET":
+				_, _ = reader.ReadString('\n')
+				key, _ := reader.ReadString('\n')
+				key = strings.TrimSpace(key)
+				_, _ = reader.ReadString('\n')
+				value, _ := reader.ReadString('\n')
+				value = strings.TrimSpace(value)
+
+				log.Println("GET: key and value are: ", key, value)
+				
+				storeMutex.Lock()
+				store[key] = value
+				storeMutex.Unlock()
+
+				conn.Write([]byte(buildRESPSimpleString("OK")))
 			}
 		}
 	}
