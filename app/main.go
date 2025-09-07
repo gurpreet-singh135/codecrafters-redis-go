f06714c6507a85831437f08f6c1441807626a714
gurpreet-singh135 <gsingh2@cs.iitr.ac.in>
2025-09-08 00:02:31 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/codecrafters-io/redis-starter-go/app/config"
+	"github.com/codecrafters-io/redis-starter-go/app/server"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
-
-	"github.com/codecrafters-io/redis-starter-go/app/config"
-	"github.com/codecrafters-io/redis-starter-go/app/server"
 )
 
 func main() {
 	fmt.Println("Starting Redis server...")
+	var portFlag string
+	flag.StringVar(&portFlag, "port", "6379", "This flag is used for specifying the port")
+
+	flag.Parse()
 
 	// Create and start the Redis server
-	redisServer := server.NewRedisServer(config.DefaultAddress)
+	redisServer := server.NewRedisServer(config.DefaultHost + ":" + portFlag)
 
 	// Handle graceful shutdown
 	go func() {
