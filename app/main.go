36880b5ae61d19d629008ef04bcb4622db626825
gurpreet-singh135 <list>
2025-09-08 22:24:47 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,26 +4,64 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/codecrafters-io/redis-starter-go/app/config"
+	"github.com/codecrafters-io/redis-starter-go/app/protocol"
 	"github.com/codecrafters-io/redis-starter-go/app/server"
 	"github.com/codecrafters-io/redis-starter-go/app/types"
+	"github.com/codecrafters-io/redis-starter-go/app/utility"
 )
 
-func processServerMetadata() (*types.ServerMetadata, string) {
-	var portFlag, replicaOf, role string
+func ConnectToPrimaryInstance(address string) {
+	// Connect to the TCP server
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		return
+	}
+	defer conn.Close() // Close the connection when the main function exits
+
+	// Send data to the server
+	message := make([]any, 0)
+	message = append(message, "PING")
+	_, err = conn.Write([]byte(protocol.BuildArray(message)))
+	if err != nil {
+		fmt.Println("Error sending data:", err)
+		return
+	}
+	fmt.Printf("Sent: %s", message)
+
+	// Receive response from the server
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+	response := string(buffer[:n])
+	fmt.Printf("Received: %s", response)
+}
+
+func ProcessServerMetadata() (*types.ServerMetadata, string) {
+	var portFlag, replicaOf, role, master_replid string
 	flag.StringVar(&portFlag, "port", "6379", "This flag is used for specifying the port")
 	flag.StringVar(&replicaOf, "replicaof", "", "This flag is used to metion the master redis instance")
 	flag.Parse()
 	if replicaOf == "" {
 		role = "master"
+		master_replid = utility.GenerateRandomString(40)
 	} else {
+		addrs := strings.Split(replicaOf, " ")
+		ConnectToPrimaryInstance(addrs[0] + ":" + addrs[1])
 		role = "slave"
 	}
 	metadata := types.NewServerMetadata(role)
+	metadata.MasterReplID = master_replid
 
 	return metadata, config.DefaultHost + ":" + portFlag
 }
@@ -31,7 +69,7 @@ func processServerMetadata() (*types.ServerMetadata, string) {
 func main() {
 	fmt.Println("Starting Redis server...")
 
-	metadata, address := processServerMetadata()
+	metadata, address := ProcessServerMetadata()
 	flag.Parse()
 
 	// Create and start the Redis server
---
+++ app/utility/util.go
@@ -0,0 +1,16 @@
+package utility
+
+import (
+	"math/rand"
+	"time"
+)
+
+func GenerateRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano())) // Seed the random number generator
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
